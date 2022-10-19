c0089a4d836f7d71c37139a01fc15111608e888a
Kritika <kritsme963@gmail.com>
2022-10-18 21:24:29 -0700

    Added:Snake ladder program

---
+++ src/com/bridglabz/snakeladder/SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.bridglabz.snakeladder;
+//player roll the die to get a number between 1 to 6,finda safe place ,
+//ladder or snake keeps doing till winning spot 100 is achieved
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake ladder simulator");
+
+    }
+}
