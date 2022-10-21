2e23847de91174bfd9b4ccb27f8a463341d3833e
Kritika <kritsme963@gmail.com>
2022-10-20 21:42:48 -0700

    Added:check option among three - no play , ladder and snake

--- src/com/bridglabz/snakeladder/SnakeLadder.java
+++ src/com/bridglabz/snakeladder/SnakeLadder.java
@@ -10,6 +10,37 @@ public class SnakeLadder {
         int dieValue=(int)(Math.random() * 6) + 1;;
         return dieValue;
     }
+    // UC3-player check for a option AMONG 3 OPTION - NO PLAY,LADDER AND SNAKE
+    static final int NO_PLAY=1;
+
+    static final int Ladder=2;
+
+    static final int Snake=3;
+    private static int getOption() {
+        int option =(int)(Math.random()*3)+1;
+        return option;
+    }
+    private static void playGame(int option,int dieValue) {
+
+        switch(option) {
+
+            case NO_PLAY:
+                System.out.println("No Play");
+                break;
+
+            case Ladder:
+                playerPosition=playerPosition + dieValue;
+                break;
+
+            case Snake:
+                playerPosition=playerPosition-dieValue;
+                break;
+
+            default:
+
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to snake ladder simulator");
     }
