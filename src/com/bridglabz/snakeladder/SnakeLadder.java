6d43209db11006f8dc55bcdb0c9fdf7d90ebad25
Kritika <kritsme963@gmail.com>
2022-10-20 21:31:12 -0700

    Added:player roll the dice to get between 1 and 6

--- src/com/bridglabz/snakeladder/SnakeLadder.java
+++ src/com/bridglabz/snakeladder/SnakeLadder.java
@@ -5,8 +5,18 @@ package com.bridglabz.snakeladder;
 public class SnakeLadder {
     //uc1-snake ladder game played with single player at start position
     static int playerPosition=0; // snake and ladder game played with single player at start position
+    //UC2-player roll the dice to get a number between  1 and 6
+    private static int dieRoll() {
+        int dieValue=(int)(Math.random() * 6) + 1;;
+        return dieValue;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to snake ladder simulator");
     }
 }
+
+
+
+
+
 
