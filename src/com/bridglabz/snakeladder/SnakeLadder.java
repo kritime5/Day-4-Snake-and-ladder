b5420764dc5c4d35e13ca9128fa4e56c08f96707
Kritika <kritsme963@gmail.com>
2022-10-20 21:16:20 -0700

    Added:game start with single player at start position zero

--- src/com/bridglabz/snakeladder/SnakeLadder.java
+++ src/com/bridglabz/snakeladder/SnakeLadder.java
@@ -3,8 +3,10 @@ package com.bridglabz.snakeladder;
 //ladder or snake keeps doing till winning spot 100 is achieved
 
 public class SnakeLadder {
+    //uc1-snake ladder game played with single player at start position
+    static int playerPosition=0; // snake and ladder game played with single player at start position
     public static void main(String[] args) {
         System.out.println("Welcome to snake ladder simulator");
-
     }
 }
+
